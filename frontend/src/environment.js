136294512f1abb38b0ed715a5e0762dfa9a041c5
Trinkesh Nimje <trinkeshnimje236@gmail.com>
2026-05-28 00:39:51 +0530

    update backend URL to render

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -2,7 +2,7 @@ let IS_PROD = true;
 const server = IS_PROD ?
     "https://apnacollegebackend.onrender.com" :
 
-    "http://localhost:8000"
+    "https://zoom-backend-dpts.onrender.com"
 
 
 export default server;
